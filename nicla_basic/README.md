5aa3e8d7e3a419bfcb1f56f4d1fb87111e51a66f
Vignesh Selvaraj <vselvaraj@wisc.edu>
2023-05-06 15:41:30 -0500

    adding ble
    Start of adding BLE connection to the NiclaSenseME device

---
+++ nicla_basic/README.md
@@ -0,0 +1,31 @@
+
+# Interfacing NiclaSenseME
+
+
+## Overview
+
+
+Interfacing with NiclaSenseME with Zephyr RTOS
+
+
+## Building and Running
+
+1. Use nrfConnectSDK for vs-code.
+
+2. Flashing the NiclaSenseME - CMSIS-DAP Link
+   1. Connect the device using USB
+   2. Install pyocd 
+
+```python
+pyocd flash -t nrf52832 ./build/zephyr/zephry.hex
+```
+
+## Serial output
+
+For serial output use minicom
+
+```shell
+minicom -b 115200 -D /dev/cu.usbmodem90CFCB952
+```
+
+Note: The device name after the flag -D can be different depending on you pc. This is your device name or serial port to which the NiclaSenseME is connected on your PC.
--- nicla_basic/src/main.cpp
+++ nicla_basic/src/main.cpp
@@ -1,6 +1,11 @@
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/fs/fs.h>
+// BLE Connection
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/gap.h>
+#include <zephyr/bluetooth/uuid.h>
+#include <zephyr/bluetooth/addr.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -8,11 +13,33 @@
 #include "NiclaSystem.hpp"
 
 
-#define TEST_FILE_SIZE 547
-#define PARTITION_NODE DT_NODELABEL(lfs1)
+#define DEVICE_NAME                 CONFIG_BT_DEVICE_NAME
+#define DEVICE_NAME_LEN             (sizeof(DEVICE_NAME) - 1)
+#define TEST_FILE_SIZE              547
+#define PARTITION_NODE              DT_NODELABEL(lfs1)
 
 
 LOG_MODULE_REGISTER(main);
+
+
+/*
+Bluetooth Connection
+*/
+// Advertising parameters
+static struct bt_le_adv_param *adv_param = BT_LE_ADV_PARAM((BT_LE_ADV_OPT_CONNECTABLE | BT_LE_ADV_OPT_USE_IDENTITY), 
+                    800,
+                    801,
+                    NULL);
+// Advertising and Scanning data
+// Advertising data
+static const struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
+    BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN),
+};
+// Scanning response data
+static const struct bt_data sd[] = {
+    BT_DATA_BYTES(BT_DATA_UUID128_ALL, BT_UUID_128_ENCODE(0x00001523, 0x1212, 0xefde, 0x1523, 0x785feabcd123))
+};
 
 
 // Create the file mount point
@@ -28,7 +55,7 @@ int main(void) {
     struct fs_statvfs sbuf;
     int rc;
 
-    LOG_PRINTK("Testing the flash read and write\n");
+    LOG_INF("Testing the flash read and write\n");
 
 
     if(FSTAB_ENTRY_DT_MOUNT_FLAGS(PARTITION_NODE) & FS_MOUNT_FLAG_AUTOMOUNT) {
