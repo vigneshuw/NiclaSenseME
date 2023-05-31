9b3b2b516eefbb17995295c567603b1d9b0e4180
Vignesh Selvaraj <vselvaraj@wisc.edu>
2023-05-31 13:26:03 -0500

    MCUBoot swap working between primary-secondary
    The bootloader is operational. The transfer between the primary and secondary partition is working.
    Challenges:
    1. If the secondary firmware is corrupted, the primary will become unrecoverable.

--- nicla_basic/src/main.cpp
+++ nicla_basic/src/main.cpp
@@ -1,15 +1,14 @@
 #include <zephyr/logging/log.h>
+#include <zephyr/kernel.h>
 #include <zephyr/dfu/flash_img.h>
 #include <zephyr/storage/flash_map.h>
 #include <zephyr/dfu/mcuboot.h>
 #include "NiclaFW/nicla_fw.h"
 
-#include "NiclaSystem.hpp"
-
 
 // FW files
-#define nicla_fw                zephyr_bin
-#define nicla_fw_len            zephyr_bin_len
+#define nicla_fw                app_update_bin
+#define nicla_fw_len            app_update_bin_len
 
 // Get uploadable data size
 uint32_t chunks = nicla_fw_len / 512;
@@ -29,18 +28,9 @@ LOG_MODULE_REGISTER(main, CONFIG_SET_LOG_LEVEL);
 
 
 struct flash_img_context flash_ctx;
+struct mcuboot_img_header header;
 
 int main(void) {
-
-    /*
-    Initialize Nicla System
-    */
-    nicla::leds.begin();
-    nicla::pmic.enableCharge(100);
-    bool ret = nicla::pmic.enable3V3LDO();
-    if(!ret) {
-        LOG_ERR("3V3LDO failed!\n");
-    }
     
     // Flash IMG init
     int rc = flash_img_init_id(&flash_ctx, UPLOAD_FLASH_AREA_ID);
@@ -55,37 +45,64 @@ int main(void) {
     }
     printk("The Original CRC is %u\n", original_crc);
 
-    
-    // // Go through chunk by chunk and write to flash
-    // size_t indexer = 0;
-    // for(size_t i = 0; i < chunks; i++) {
-
-    //     indexer = i * 512;
-
-    //     bytecpy(buf, &nicla_fw[indexer], 512);
-    //     // Write the buf to flash
-    //     rc = flash_img_buffered_write(&flash_ctx, buf, 512, false);
-    //     if(rc) {
-    //         printk("Failed to write on flash, rc = %d", rc);
-    //     }
-    // }
-    // indexer += 512;
-    // memset(buf, 0, chunks_remain);
-    // // Write the remainder
-    // bytecpy(buf, &nicla_fw[indexer], chunks_remain);
-    // // Write the buf to flash
-    // rc = flash_img_buffered_write(&flash_ctx, buf, chunks_remain, true);
+    // rc = flash_img_buffered_write(&flash_ctx, nicla_fw, nicla_fw_len, true);
     // if(rc) {
     //     printk("Failed to write on flash, rc = %d", rc);
     // }
+    
+    // Go through chunk by chunk and write to flash
+    size_t indexer = 0;
+    for(size_t i = 0; i < chunks; i++) {
 
+        indexer = i * 512;
 
+        bytecpy(buf, &nicla_fw[indexer], 512);
+        // Write the buf to flash
+        rc = flash_img_buffered_write(&flash_ctx, buf, 512, false);
+        if(rc) {
+            printk("Failed to write on flash, rc = %d", rc);
+        }
+    }
+    indexer += 512;
+    memset(buf, 0, chunks_remain);
+    // Write the remainder
+    bytecpy(buf, &nicla_fw[indexer], chunks_remain);
+    // Write the buf to flash
+    rc = flash_img_buffered_write(&flash_ctx, buf, chunks_remain, true);
+    if(rc) {
+        printk("Failed to write on flash, rc = %d", rc);
+    }
 
+    // Print the number of bytes written
+    size_t bytes_written = flash_img_bytes_written(&flash_ctx);
+    printk("The total number of bytes written is %u\n", bytes_written);
 
+    // // Erasing the image bank 
+    // rc = boot_erase_img_bank(UPLOAD_FLASH_AREA_ID);
+    // if(rc) {
+    //    printk("Failed to boot erase on flash, rc = %d\n", rc); 
+    // } else {
+    //     printk("Erase successful\n");
+    // }
+
+    // Read the header information for an image
+    rc = boot_read_bank_header(UPLOAD_FLASH_AREA_ID, &header, sizeof(header));
+    if(rc) {
+        printk("Failed to read the image header, rc = %d\n", rc);
+    }
+
+    // Start this new image
+    rc = boot_request_upgrade(BOOT_UPGRADE_TEST);
+    if(rc) {
+        printk("Boot upgrade request has failed\n");
+    } else {
+        printk("Boot upgrade request is successful\n");
+    }
     while (1) {
         
-        k_msleep(1000);
-        printk(".\n");
+        k_msleep(5000);
+        // Print image information
+        printk("Image Magic, %u\n", header.mcuboot_version);
 
     }
 
