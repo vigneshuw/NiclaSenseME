7245cb3b7147e847df83462bdad30db944096d55
Vignesh Selvaraj <vselvaraj@wisc.edu>
2023-05-31 00:33:31 -0500

    testing flash img write

--- nicla_basic/src/main.cpp
+++ nicla_basic/src/main.cpp
@@ -1,15 +1,34 @@
 #include <zephyr/logging/log.h>
+#include <zephyr/dfu/flash_img.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/dfu/mcuboot.h>
+#include "NiclaFW/nicla_fw.h"
 
 #include "NiclaSystem.hpp"
-#include "BHY2.h"
+
+
+// FW files
+#define nicla_fw                zephyr_bin
+#define nicla_fw_len            zephyr_bin_len
+
+// Get uploadable data size
+uint32_t chunks = nicla_fw_len / 512;
+uint32_t chunks_remain = nicla_fw_len % 512;
+// FW chunk data buffer
+uint8_t buf[512];
+
+// Node label
+#define UPLOAD_FLASH_AREA_LABEL             slot1_partition
+// Flash area ID
+#define UPLOAD_FLASH_AREA_ID                FIXED_PARTITION_ID(UPLOAD_FLASH_AREA_LABEL)
+// Flash area controller
+#define UPLOAD_FLASH_AREA_CONTROLLER        DT_GPARENT(DT_NODELABEL(UPLOAD_FLASH_AREA_LABEL))
 
 
 LOG_MODULE_REGISTER(main, CONFIG_SET_LOG_LEVEL);
 
 
-// Acceleration sensor
-SensorXYZ accel(SENSOR_ID_ACC_PASS);
-
+struct flash_img_context flash_ctx;
 
 int main(void) {
 
@@ -23,14 +42,50 @@ int main(void) {
         LOG_ERR("3V3LDO failed!\n");
     }
     
-    // Initialize
-    bhy2.begin();
-    accel.begin(400.0, 0);
+    // Flash IMG init
+    int rc = flash_img_init_id(&flash_ctx, UPLOAD_FLASH_AREA_ID);
+    if (rc) {
+        printk("Failed to init flash img, rc = %d\n", rc);
+    }
+
+    // Compute the CRC
+    uint8_t original_crc = 0x00;
+    for(unsigned int i = 0; i < nicla_fw_len; i++) {
+        original_crc = original_crc ^ nicla_fw[i];
+    }
+    printk("The Original CRC is %u\n", original_crc);
+
+    
+    // // Go through chunk by chunk and write to flash
+    // size_t indexer = 0;
+    // for(size_t i = 0; i < chunks; i++) {
+
+    //     indexer = i * 512;
+
+    //     bytecpy(buf, &nicla_fw[indexer], 512);
+    //     // Write the buf to flash
+    //     rc = flash_img_buffered_write(&flash_ctx, buf, 512, false);
+    //     if(rc) {
+    //         printk("Failed to write on flash, rc = %d", rc);
+    //     }
+    // }
+    // indexer += 512;
+    // memset(buf, 0, chunks_remain);
+    // // Write the remainder
+    // bytecpy(buf, &nicla_fw[indexer], chunks_remain);
+    // // Write the buf to flash
+    // rc = flash_img_buffered_write(&flash_ctx, buf, chunks_remain, true);
+    // if(rc) {
+    //     printk("Failed to write on flash, rc = %d", rc);
+    // }
+
+
+
 
     while (1) {
         
-        bhy2.update(1000);
-        printk("X-%d, Y-%d, Z-%d\n", accel.x(), accel.y(), accel.z());
+        k_msleep(1000);
+        printk(".\n");
 
     }
 
