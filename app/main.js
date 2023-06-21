23f1e08abea13a8ee12f4eeabc1b76aa4c840b1d
Vignesh Selvaraj <vselvaraj@wisc.edu>
2023-06-21 10:20:15 -0500

    create the design of fw update page

--- app/main.js
+++ app/main.js
@@ -1,24 +1,38 @@
-const {app, BrowserWindow} = require("electron")
+const {app, BrowserWindow, dialog} = require("electron")
 // The main window
 let mainWindow
+
+// BLE device connections
+let is_device_connected = false
 
 function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1200, height: 800,
-        webPreferences: {nodeIntegration: true}
+        webPreferences: {nodeIntegration: true},
+        resizable: false,
+        center: true,
+        show: false,
+        maximizable: false
     })
 
     // Load some HTML into main Window
     mainWindow.loadFile("./index.html")
+        .then(() => {
+            mainWindow.once('ready-to-show', mainWindow.show)
+        })
+
 
     // On closing the window
     mainWindow.on("closed", () => {
-
-        // Disconnect the BLE and wrap-up
-
         mainWindow = null
     })
 }
 
 // When the app is ready
 app.on('ready', createWindow)
+
+// Quitting the app
+app.on('before-quit', () => {
+    // Disconnect the BLE and wrap-up
+
+})
